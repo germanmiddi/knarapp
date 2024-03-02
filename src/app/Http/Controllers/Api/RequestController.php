2c680ec3b24449eb4a83b00dcbc5a7bdff57cf88
German Middi <german@onmedia.com.ar>
2024-03-02 23:48:37 +1100

    .

--- src/app/Http/Controllers/Api/RequestController.php
+++ src/app/Http/Controllers/Api/RequestController.php
@@ -24,20 +24,47 @@ class RequestController extends Controller
                 'date' => '01/10/2023',
                 'time' => '09:00',
                 'location_from' => [
-                    'name' => 'Aeropuerto Eziza',
-                    'lat' => -34.822222,
-                    'lng' => -58.535833
+                    'name' => 'Aeropuerto Josep Tarradellas',
+                    'lat' =>  41.301908, 
+                    'lng' => 2.072612
                 ],
                 'location_to' => [
-                    'name' => 'Hotel Panamericano',
-                    'lat' => -34.609964,
-                    'lng' => -58.375181
+                    'name' => 'Holiday Inn Express Barcelona',
+                    'lat' => 41.40203324311969, 
+                    'lng' => 2.198810284486863
                 ],
                 'status' => 'pending',
                 'flight_number' => 'AA 1234'
             ],
             [
                 'id' => 2,
+                'service_type' => 'Transfer',
+                'service_name' => 'Transfer Ezeiza / Hotel Céntrico o Viceversa',
+                'company' => '',
+                'cant_pax' => '3',
+                'pax_contact' => [
+                    'name' => '',
+                    'phone' => '',
+                    'email' => ''
+                ],
+                'date' => '15/10/2023',
+                'time' => '20:00',
+                'location_from' => [
+                    'name' => 'Hotel Panamericano',
+                    'lat' => -34.609964,
+                    'lng' => -58.375181
+                ],
+                'location_to' => [
+                    'name' => 'Aeropuerto Eziza',
+                    'lat' => -34.822222,
+                    'lng' => -58.535833
+                ],
+                'status' => 'pending',
+                'flight_number' => '',
+                'passengers' => 1
+            ],
+            [
+                'id' => 3,
                 'service_type' => 'Transfer',
                 'service_name' => 'Transfer Ezeiza / Hotel Céntrico o Viceversa',
                 'company' => '',
