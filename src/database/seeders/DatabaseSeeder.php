b1409c238194060fcb12acca3397c353ec598267
German Middi <german@onmedia.com.ar>
2024-03-02 20:52:48 +1100

    .

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -101,9 +101,9 @@ class DatabaseSeeder extends Seeder
             'created_at' => Carbon::now(),
         ]);
 
-        
-
-=        
+        $this->call([
+            LocationSeeder::class
+        ]); 
 
     }
 }
